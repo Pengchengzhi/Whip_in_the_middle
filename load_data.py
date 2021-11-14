10c063a5b8499ffb154c303801e3bee941eb9bfb
yuchuw <60901953+yuchuw@users.noreply.github.com>
2021-11-13 20:44:46 -0500

    Update load_data.py

--- load_data.py
+++ load_data.py
@@ -10,6 +10,22 @@ from PIL import Image
 import glob
 from skimage import color
 
+
+''' Example of dataloader in the main.py
+from load_data import Imgnet_Dataset as myDataset
+from torch.utils import data
+
+data_root = './'          # Working directory
+data_train = myDataset(data_root, shuffle = True, size = 256, mode = 'train')
+train_loader = data.DataLoader(data_train, batch_size = 10, shuffle = False)
+
+data_test = myDataset(data_root, shuffle = True, size = 256, mode = 'test')
+test_loader = data.DataLoader(data_train, batch_size = 10, shuffle = False)
+
+for i, (data, label) in enumerate(train_loader):
+    print(label)
+'''
+
 #### Definition of loader
 def pil_loader(path):
     with open(path, 'rb') as f:
